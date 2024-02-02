1bd3c2f56ba4f9c90e6af45c1a622e6342841560
rzlwldn <rizal.wildan@tlab.co.id>
2024-02-02 08:50:19 +0700

    Finish static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   def home
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
