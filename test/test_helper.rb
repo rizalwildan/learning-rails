73e3eed4b640063e30e2b722385f47ba9f1c63d3
rzlwldn <rizal.wildan@tlab.co.id>
2024-02-04 22:26:30 +0700

    Add remember login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 module ActiveSupport
-  class TestCase
+  class ActiveSupport::TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
@@ -16,6 +16,22 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
     def is_logged_in?
       !session[:user_id].nil?
+    end
+
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+  end
+
+  class ActionDispatch::IntegrationTest
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: {
+        session: {
+          email: user.email,
+          password: password,
+          remember_me: remember_me
+        }
+      }
     end
   end
 end
